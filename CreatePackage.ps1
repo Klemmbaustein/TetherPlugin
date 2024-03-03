ad1067d3e6ae35c116418106fa937ea0fd8c3375
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2024-03-03 22:00:58 +0100

    update build 2

--- CreatePackage.ps1
+++ CreatePackage.ps1
@@ -8,7 +8,11 @@ cp TetherInstaller/NorthstarInstaller/Data Build/$package_name/plugins/bin -r -f
 
 cp -r -fo GameScripts/* Build/$package_name/mods/Klemmbaustein.Tether/
 
-if ($args[1] -ne "")
+$profile_name = $args[1]
+$game_dir = $args[0]
+
+if ($profile_name -ne "")
 {
-	cp -r -fo Build/$package_name $args[0]\$args[1]\packages\
+	echo "Installing to $game_dir\$profile_name"
+	cp -r -fo Build/$package_name $game_dir\$profile_name\packages\
 }
